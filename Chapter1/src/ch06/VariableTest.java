d0368812b9288dbd4a59cd27c4b3f6bb02afa3bd
seojun <seojun1208@gmail.com>
2021-10-12 18:33:01 +0900

    Initial commit

---
+++ Chapter1/src/ch06/VariableTest.java
@@ -0,0 +1,14 @@
+package ch06;
+
+public class VariableTest {
+
+    public static void main(String[] args) {
+
+        byte bNum = 127;
+        System.out.println(bNum);
+
+//        int num = 12345678900;
+        long lNUm = 12345678900L;
+        System.out.println(lNUm);
+    }
+}
---
+++ Chapter1/src/ch07/DoubleTest.java
@@ -0,0 +1,13 @@
+package ch07;
+
+public class DoubleTest {
+
+    public static void main(String[] args) {
+
+        double dNum = 3.14;
+        float fNum = 3.14F;
+
+        System.out.println(dNum);
+        System.out.println(fNum);
+    }
+}
