731e704a0e54861b5a42857672a30fbebce1e401
seojun <seojun1208@gmail.com>
2021-10-13 15:57:53 +0900

    java 기초

---
+++ Chapter1/src/ch10/OperatorTest.java
@@ -0,0 +1,13 @@
+package ch10;
+
+public class OperatorTest {
+
+    public static void main(String[] args) {
+
+        int gameScore = 150;
+        int lastScore = gameScore++;
+
+        System.out.println(lastScore);
+        System.out.println(gameScore);
+    }
+}
