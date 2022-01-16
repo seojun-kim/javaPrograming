028ee288e551333bf70b137a50461ea990fded38
seojun <seojun1208@gmail.com>
2022-01-17 00:05:35 +0900

    exception

---
+++ Chapter6/src/ch09/ArrayIndexException.java
@@ -0,0 +1,17 @@
+package ch09;
+
+public class ArrayIndexException {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        try {
+            for(int i = 0; i <= 5; i++) {
+                System.out.println(arr[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
---
+++ Chapter6/src/ch09/FileExceptionHandling.java
@@ -0,0 +1,9 @@
+package ch09;
+
+public class FileExceptionHandling {
+
+    public static void main(String[] args) {
+
+
+    }
+}
